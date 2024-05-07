69cc817de0ba3bcb0af7001887a109df883aac57
sofa1654 <klimkinasofa@gmail.com>
2024-05-07 23:06:46 +0300

    v1-0-1

---
+++ Calc.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream> 
+
+int main()
+{
+	float a;
+	float b;
+	char sign;
+	std::cin >> a;
+	std::cin >> sign;
+	std::cin >> b;
+	if (sign == '+') {
+		std::cout << a + b;
+	}
+	else if (sign == '-') {
+		std::cout << a - b;
+	}
+	else if (sign == '*') {
+		std::cout << a * b;
+	}
+	else if (sign == '/' || sign == ':') {
+		std::cout << a / b;
+	}
+}
